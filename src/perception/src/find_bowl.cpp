813ae1c8860a9eb4917bd28114cf0b66649547a6
Kieran Haden <um10kh@leeds.ac.uk>
2016-02-09 17:42:13 +0000

    Edited find bowl file by adding comments - for use in meeting on 10/02/16

--- src/perception/src/find_bowl.cpp
+++ src/perception/src/find_bowl.cpp
@@ -35,47 +35,57 @@ Eigen::Affine3d create_rotation_matrix(double ax, double ay, double az) {
   return rz * ry * rx;
 }
 
+// Create a callback method on the receipt of a PointCloud2 object
 void
 cloud_cb (const sensor_msgs::PointCloud2 cloud_msg)
 {
   std::cout << cloud_msg.header.frame_id << std::endl;
-  pcl::PointCloud<pcl::PointXYZ> cloud3;
-  pcl::fromROSMsg (cloud_msg, cloud3);
 
-  std::cout << cloud3.header.frame_id << std::endl;
+  // Convert the PointCloud2 to a PCL cloud
+  pcl::PointCloud<pcl::PointXYZ> pcl_cloud;
+  pcl::fromROSMsg (cloud_msg, pcl_cloud);
 
+  std::cout << pcl_cloud.header.frame_id << std::endl;
 
-  //pcl::io::savePCDFileASCII ("circle_found.pcd", cloud3);
+  // Save PCL cloud as an ASCII file
+  //pcl::io::savePCDFileASCII ("circle_found.pcd", pcl_cloud);
 
+  // Create model variables to store the circle model values in
   pcl::ModelCoefficients coefficients;
   pcl::PointIndices inliers;
   // Create the segmentation object
   pcl::SACSegmentation<pcl::PointXYZ> seg;
-  // Optional
   seg.setOptimizeCoefficients (true);
-  // Mandatory
+  // Using RANSAC to find a circle within a 2D space
   seg.setModelType (pcl::SACMODEL_CIRCLE3D);
   seg.setMethodType (pcl::SAC_RANSAC);
   seg.setMaxIterations(1000);
+  // Max distance of 1 cm between points
   seg.setDistanceThreshold (0.01);
+  // Set limit on size of bowl - not search too big or too small circles
   seg.setRadiusLimits(0.05,0.12);
 
-  seg.setInputCloud (cloud3.makeShared());
+  // Set the PCL cloud as the input and segment into the inliers/coefficients
+  seg.setInputCloud (pcl_cloud.makeShared());
   seg.segment (inliers, coefficients);
 
+  // Convert the model coefficients to a set of publishable coefficients
   pcl_msgs::ModelCoefficients ros_coefficients;
   pcl_conversions::fromPCL(coefficients, ros_coefficients);
 
+  // Publish on output
   pub.publish(ros_coefficients);
 
+  // Print coefficients to terminal
   //std::cout<< ros_coefficients;
 
+  // Initialise new point cloud of just bowl rim pointcloud and output to text
+  // file
   pcl::PointCloud<pcl::PointXYZ>::Ptr final(new pcl::PointCloud<pcl::PointXYZ>);
-
-  pcl::copyPointCloud<pcl::PointXYZ>(cloud3, inliers, *final);
-
+  pcl::copyPointCloud<pcl::PointXYZ>(pcl_cloud, inliers, *final);
   pcl::io::savePCDFileASCII ("circle_found.pcd", *final);
 
+  //Using a transform listener, create a point using the coefficients
   tf::TransformListener listener;
   tf::StampedTransform transform;
 
@@ -87,6 +97,9 @@ cloud_cb (const sensor_msgs::PointCloud2 cloud_msg)
   pt.point.x = ros_coefficients.values[0];
   pt.point.y = ros_coefficients.values[1];
   pt.point.z = ros_coefficients.values[2];
+
+  // Publish the created point for viewing in rViz
+  pointPub.publish(pt);
 }
 
 
@@ -98,8 +111,8 @@ main (int argc, char** argv)
   ros::init (argc, argv, "my_pcl_tutorial");
   ros::NodeHandle nh;
 
-  // Create a ROS subscriber for the input point cloud
-  ros::Subscriber sub = nh.subscribe ("input", 1, cloud_cb);
+  // Subscribe to the table objects PointCloud2 from the kinect
+  ros::Subscriber sub = nh.subscribe ("/tabletop_pointcloud_1", 1, cloud_cb);
 
   // Create a ROS publisher for the output model coefficients
   pub = nh.advertise<pcl_msgs::ModelCoefficients> ("output", 1);
