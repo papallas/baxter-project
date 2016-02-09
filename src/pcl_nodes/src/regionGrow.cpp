9fb8b4d69e72608064f09d85f02f84240163144f
Kieran Haden <um10kh@leeds.ac.uk>
2016-02-09 16:53:27 +0000

    Initial commit of various files developed within the first two weeks of the baxter project

---
+++ src/pcl_nodes/src/regionGrow.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/search/search.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/segmentation/region_growing.h>
